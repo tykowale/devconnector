e3c6e35f1e72abb2affaf4db5c0c6c88ddfcf6ab
tykowale <tykowale@gmail.com>
2020-09-11 15:24:01 -0700

    vaidate user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator/check");
+
 const router = express.Router();
 
-// @route       GET api/users
-// @description Test route
+// @route       POST api/users
+// @description Register user
 // @access      Public
-router.get("/", (req, res) => res.send("User Route"));
+router.post("/", [
+  check("name", "Name is required").notEmpty(),
+  check("email", "Please include a valid email").isEmail(),
+  check("password", "Please enter a password with 6 or more characters")
+    .isLength({
+      min: 6,
+    }),
+], (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  console.log(req.body);
+  res.send("User Route");
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Connect Database
 connectDB();
-const PORT = process.env.PORT || 5000;
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API Running"));
 
