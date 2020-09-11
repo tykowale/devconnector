5cd3b70f6a5f409dc8dde5ce92ef6f78e2234f54
tykowale <tykowale@gmail.com>
2020-09-11 14:22:10 -0700

    add routes

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const app = express();
 
 // Connect Database
 connectDB();
-
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("API Running"));
+
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
 });
 
-app.get("/", (req, res) => res.send("API Running"));
