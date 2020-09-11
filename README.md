2496772fabf7752dd89a1c01221f7dfe09c80bab
tykowale <tykowale@gmail.com>
2020-09-11 14:13:30 -0700

    connect to mongo

---
+++ README.md
@@ -0,0 +1,10 @@
+# DevConnector
+
+Config file needs to be filled with fields and have .sample removed
+
+
+To Run:
+
+```bash
+npm run server
+```
---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+async function connectDB() {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    // Exit app with nonzero code
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
