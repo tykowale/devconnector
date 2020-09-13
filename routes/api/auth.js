c99ae4428d278bacd9a54495f4eadd8fdcd3df8d
tykowale <tykowale@gmail.com>
2020-09-13 15:53:49 -0700

    add authorized routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const router = express.Router();
 
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+
+const router = express.Router();
 // @route       GET api/auth
 // @description Test route
 // @access      Public
-router.get("/", (req, res) => res.send("Auth Route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user)
+  } catch (err) {
+    console.error(error.message);
+    res.status(500).json({msg: "Server Error"})
+  }
+});
 
 module.exports = router;
