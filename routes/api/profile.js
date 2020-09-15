30456aff61a888f4be04d4c5af83f0ef9754cbfc
tykowale <tykowale@gmail.com>
2020-09-15 11:39:28 -0700

    create profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,86 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route       GET api/profile
-// @description Test route
-// @access      Public
-router.get("/", (req, res) => res.send("Profile Route"));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route       GET api/profile/me
+// @description Get current user's profile
+// @access      Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile
+      .findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "No Profile for this User" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Sever Error");
+  }
+});
+
+// @route       POST api/profile
+// @description Create or update user profile
+// @access      Private
+router.post("/", [auth, [
+  check("status", "Status is required").notEmpty(),
+  check("skills", "Skills are required").notEmpty(),
+]], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    company,
+    location,
+    website,
+    bio,
+    skills,
+    status,
+    githubusername,
+    youtube,
+    twitter,
+    instagram,
+    linkedin,
+    facebook,
+  } = req.body;
+
+  const social = { youtube, twitter, instagram, linkedin, facebook };
+  const profileFields = {
+    user: req.user.id,
+    company,
+    location,
+    website: website,
+    bio,
+    skills: Array.isArray(skills)
+      ? skills
+      : skills.split(",").map((skill) => " " + skill.trim()),
+    status,
+    githubusername,
+    social,
+  };
+
+  try {
+    const profile = await Profile.findOneAndUpdate(
+      { user: req.user.id },
+      { $set: profileFields },
+      { new: true, upsert: true, setDefaultsOnInsert: true },
+    );
+    return res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
